ada91c3d3ca990f662c03af7bdf21c0e1b84fcdf
lien2512 <liennguyen.uet.vnu@gmail.com>
2020-02-17 15:14:56 +0700

    dev#login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { LoginService } from './services/login.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) {
+    this.loginService.restoreLogin();
+  }
+
+  ngOnInit() {
+    // Lắng nghe các thay đổi khi người dùng đăng nhập/đăng xuất, hoặc đăng nhập lại
+    this.subcribeLogin();
+  }
+
+  logout() {
+    this.loginService.logout();
+  }
+
+  subcribeLogin() {
+    this.loginService.userLogined.subscribe(
+      (curentUser) => {
+        // Nếu !curentUser: user đăng đăng nhập không tồn tại -> redirect tới login
+        if (!curentUser) { this.router.navigate(['login']); }
+
+        // TODO: Nếu người dùng đăng nhập thay đổi
+        // -> cập nhật lại các thông tin trên trang
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginService} from './services/login.service';
+import { HttpClientModule } from '@angular/common/http';
+const routerConfig: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'search', component: MainComponent},
 
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SidebarComponent,
+    MainComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+      HttpClientModule,
+      RouterModule.forRoot(routerConfig),
+    ],
+  exports: [RouterModule],
+  bootstrap: [AppComponent],
+  providers: [LoginService, HttpClientModule]
 })
 export class AppModule { }
